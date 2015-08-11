a9ff132e3e7794f06df496936a428fbfeb7a31a3
ckwer2008 <ckw-er@163.com>
2015-08-11 19:30:50 +0800

    test

--- src/main/java/com/ckw/controller/IndexController.java
+++ src/main/java/com/ckw/controller/IndexController.java
@@ -2,13 +2,13 @@ package com.ckw.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
+@RequestMapping("/index")
 public class IndexController {
 
-	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	@RequestMapping(value = "/index")
 	public String index(){
 		System.err.println("====================");
 		return "index";
